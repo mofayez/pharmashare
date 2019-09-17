25dc71ddccae7423d8d7dcd1f00668dc8ae106d9
mofayez <mohamedsy474@gmail.com>
2019-09-17 14:30:09 +0200

    2019-9/17 adds

--- app/Http/Controllers/Web/Profile/ProfileController.php
+++ app/Http/Controllers/Web/Profile/ProfileController.php
@@ -120,7 +120,6 @@ class ProfileController extends Controller
         if ($response['status']) {
             $sale = $response['data']['sale'];
         }
-//        return $sale;
         return view('pages.reports.sales.index', compact('page_title', 'user', 'all_users', 'sale'));
 
     }
